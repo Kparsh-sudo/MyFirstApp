1f336e7b92e2b0fcd7009278e876af8607490225
Admin <k.parshickov@yandex.ru>
2026-03-21 11:17:43 +0300

    Day 8: Material Design styling for buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ buildFeatures {
 dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(libs.androidx.core.ktx)
+    implementation("com.google.android.material:material:1.11.0")
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
