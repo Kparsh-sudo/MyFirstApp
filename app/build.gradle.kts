3def3486b9d10c796ed42798db1ab40d03cb9d53
Паршиков Кирилл <k.parshickov@yandex.ru>
2026-03-25 00:42:00 +0300

    Day 9: Added video support and separate edit activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,15 @@ buildFeatures {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    //kotlinOptions {
+    //       jvmTarget = "11"
+    //}
+    kotlin {
+        jvmToolchain {
+            languageVersion.set(JavaLanguageVersion.of(11))
+        }
     }
 
 }
--- app/src/main/java/ru/parshikov/myfirstapp/activity/EditPostActivity.kt
+++ app/src/main/java/ru/parshikov/myfirstapp/activity/EditPostActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import ru.parshikov.myfirstapp.R
 import ru.parshikov.myfirstapp.databinding.ActivityEditPostBinding
 
 class EditPostActivity : AppCompatActivity() {
